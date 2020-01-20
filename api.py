0ce09dcc591f2d56c0453d2ef5391f47936a6d1d
maxious <maxious@maxious-Swanky>
2020-01-20 21:06:08 +1100

    add dynamodb beginnings

--- api.py
+++ api.py
@@ -1,35 +1,25 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import json
 
 
-def hello(event, context):
+def get_latest(event, context):
 
-    # For a Boto3 client.
-    ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
-    response = ddb.list_tables()
-    print(response)
-
-    # For a Boto3 service resource
     ddb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
-    print(list(ddb.tables.all()))
 
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": event,
+        "tables": list(ddb.tables.all())
     }
-
+# response = table.query(
+#     KeyConditionExpression=Key('username').eq('johndoe')
+# )
+# items = response['Items']
+# print(items)
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
--- archiver.py
+++ archiver.py
@@ -2,7 +2,6 @@ import httpx
 import arrow
 import orjson
 import pathlib
-RFC822_DATE = "ddd, DD MMM YYYY HH:mm:ss ZZZ"
 minLat = -36
 maxLat = -34
 minLon = 148
@@ -10,45 +9,48 @@ maxLon = 150
 
 
 def archive_luftdaten(event, context):
-    # last_modified= "Sat, 18 Jan 2020 08:22:25 GMT"
-
-    # r = httpx.head("https://data.sensor.community/static/v2/data.dust.min.json", headers={'If-Modified-Since': last_modified})
-    # if r.status_code == 200:
-    #     print (r,r.headers)
-    #     last_modified_date = arrow.get(r.headers['Last-Modified'],RFC822_DATE)
-    #     print("Last Modified: %s" %  last_modified_date.format('DDMMYYY-HHmmss.X'))
-    #     print("")
-    #     print('last_modified= "%s"' % r.headers['Last-Modified'])
-    # else:
-    #     print ("No action for http status %d" % r.status_code)
-
-    luftdaten = orjson.loads(pathlib.Path('data.dust.min.json').read_text())
+    ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
+    metadaten = ddb.Table('metadatenTable')
+    last_modified = metadaten.get_item(Key={'metadataKey': 'last_modified'}).get(
+        'Item', {}).get('metadataValue', "Sat, 18 Jan 2020 08:22:25 GMT")
+    r = httpx.get("https://data.sensor.community/static/v2/data.dust.min.json",
+                  headers={'If-Modified-Since': last_modified})
+    if r.status_code != 200:
+        print("No action for http status %d" % r.status_code)
+        return
+    last_modified['metadataValue'] = r.headers['Last-Modified']
+    metadaten.put_item(last_modified)
+    luftdaten = orjson.loads(r.text())
+    #luftdaten = orjson.loads(pathlib.Path('data.dust.min.json').read_text())
     canberra_daten = []
     i = 0
-    for daten in luftdaten:
-        i += 1
-        if i % 1000 == 0:
-            print(i)
-        if i > 10000:
-            lat = float(daten['location']['latitude']
-                        ) if daten['location']['latitude'] != '' else 0
-            lon = float(daten['location']['longitude']
-                        ) if daten['location']['longitude'] != '' else 0
-            if minLat < lat < maxLat and minLon < lon < maxLon:
-                updateTime = arrow.get(daten['timestamp'])
-                PM10 = None
-                PM2_5 = None
-                for value in daten['sensordatavalues']:
-                    if value['value_type'] == "P1":
-                        PM10 = float(value['value'])
-                    if value['value_type'] == "P2":
-                        PM2_5 = float(value['value'])
-                datum = {"updateTime": int(updateTime.format('X')), 
-                "expiryTime": updateTime.shift(hours=+24).format('X'), "recordId": daten['id'],
-                         "locationId": daten['location']['id'], "lat": lat, "lon": lon,
-                         "PM10": PM10, "PM2_5": PM2_5}
-                # print(daten)
-                print(datum)
+    with ddb.Table('luftdatenTable').batch_writer() as batch:
+        for daten in luftdaten:
+            i += 1
+            if i % 1000 == 0:
+                print(i)
+            if i > 10000:
+                lat = float(daten['location']['latitude']
+                            ) if daten['location']['latitude'] != '' else 0
+                lon = float(daten['location']['longitude']
+                            ) if daten['location']['longitude'] != '' else 0
+                if minLat < lat < maxLat and minLon < lon < maxLon:
+                    updateTime = arrow.get(daten['timestamp'])
+                    PM10 = None
+                    PM2_5 = None
+                    for value in daten['sensordatavalues']:
+                        if value['value_type'] == "P1":
+                            PM10 = float(value['value'])
+                        if value['value_type'] == "P2":
+                            PM2_5 = float(value['value'])
+                    datum = {"updateTime": int(updateTime.format('X')),
+                             "expiryTime": updateTime.shift(hours=+24).format('X'), "recordId": daten['id'],
+                             "locationId": daten['location']['id'], "lat": lat, "lon": lon,
+                             "PM10": PM10, "PM2_5": PM2_5}
+                    # print(daten)
+                    print(datum)
+                    canberra_daten.append(datum)
+                    batch.put_item(Item=datum)
     body = {
         "message": "Archived!",
         "input": event
