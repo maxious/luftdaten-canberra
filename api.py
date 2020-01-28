5b48ee742080dac25afcdacf1e3d9b5dab31ed18
maxious <maxious@maxious-Swanky>
2020-01-28 22:03:46 +1100

    nearest index value?

--- api.py
+++ api.py
@@ -7,12 +7,14 @@ import pandas as pd
 
 PROD = os.environ.get('SERVERLESS_STAGE', None) == 'prod'
 
-# TODO 24 hour history using table scan, grouped by sensor
+# TODO 24 hour history using table scan, grouped by sensor/time bin
 # https://www.earthdatascience.org/courses/use-data-open-source-python/use-time-series-data-in-python/resample-time-series-data-pandas-python/
 # https://stackoverflow.com/questions/45156289/pandas-where-is-the-documentation-for-timegrouper
 
+
 def get_daily_history(event, context):
-    ddb = boto3.resource('dynamodb', endpoint_url=(None if PROD else 'http://localhost:8000'))
+    ddb = boto3.resource('dynamodb', endpoint_url=(
+        None if PROD else 'http://localhost:8000'))
     day_ago = int(arrow.now().shift(hours=-24).format('X'))
     response = ddb.Table('luftdatenTable').query(
         KeyConditionExpression=Key('luftdaten').eq(1)
@@ -37,21 +39,26 @@ def get_daily_history(event, context):
 
 
 def _items_to_location_dataframe(items):
-    df = pd.DataFrame.from_records(items, exclude=['recordId','luftdaten','expiryTime'])
-    locations = (df.groupby('locationId')[['lat','lon']].max().to_dict(orient="index"))
+    df = pd.DataFrame.from_records(
+        items, exclude=['recordId', 'luftdaten', 'expiryTime'])
+    locations = (df.groupby('locationId')[
+                 ['lat', 'lon']].max().to_dict(orient="index"))
     df = df.drop('lat', axis=1).drop('lon', axis=1)
     df['datetime'] = pd.to_datetime(df['updateTime'].astype('int'), unit='s')
     df = df.set_index('datetime')
     df.drop(['updateTime'], axis=1, inplace=True)
-    #df.to_csv('records.csv')
+    df["PM10"] = pd.to_numeric(df["PM10"])
+    df["PM2_5"] = pd.to_numeric(df["PM2_5"])
+    df.to_csv('records.csv')
     return locations, df
+
+
 def get_latest(event, context):
 
-    ddb = boto3.resource('dynamodb', endpoint_url=(None if PROD else 'http://localhost:8000'))
+    ddb = boto3.resource('dynamodb', endpoint_url=(
+        None if PROD else 'http://localhost:8000'))
     now = arrow.now() if PROD else arrow.get(1579695768)
     three_quarter_hour_ago = int(now.shift(minutes=-45).format('X'))
-    # half_hour_ago = int(now.shift(minutes=-30).format('X'))
-    # quarter_hour_ago = int(now.shift(minutes=-15).format('X'))
     response = ddb.Table('luftdatenTable').query(
         KeyConditionExpression=Key('luftdaten').eq(1)
         & Key('updateTime').gt(three_quarter_hour_ago if PROD else 1)
@@ -59,37 +66,35 @@ def get_latest(event, context):
     result = {}
     data = {}
     locations, df = _items_to_location_dataframe(response['Items'])
-    # for item in response['Items']:
-    #     locationId = int(item['locationId'])
-    #     if locationId not in result:
-    #         result[locationId] = {'lat': float(
-    #             item['lat']), 'lon': float(item['lon'])}
-    #     if locationId not in data:
-    #         data[locationId] = {
-    #             '30min_PM10': [], '30min_PM2_5': [], 
-    #                '10min_PM10': [], '10min_PM2_5': [], 
-    #         'latest_time': 0}
-    #     if int(item['updateTime']) > half_hour_ago:
-    #         data[locationId]['current_PM10'][int(
-    #             item['updateTime'])] = float(item['PM10'])
-    #         result[locationId]['current_PM2_5'][int(
-    #             item['updateTime'])] = float(item['PM2_5'])
-    #         data[locationId]['latest_time'] = int(item['updateTime'])
-    #     if int(item['updateTime']) > data[locationId].get('latest_time', 0):
-    #         result[locationId]['current_PM10'][int(
-    #             item['updateTime'])] = float(item['PM10'])
-    #         result[locationId]['current_PM2_5'][int(
-    #             item['updateTime'])] = float(item['PM2_5'])
-    #         data[locationId]['latest_time'] = int(item['updateTime'])
-    #     if int(item['updateTime']) > data[locationId].get('latest_time', 0):
-    #         result[locationId]['current_PM10'][int(
-    #             item['updateTime'])] = float(item['PM10'])
-    #         result[locationId]['current_PM2_5'][int(
-    #             item['updateTime'])] = float(item['PM2_5'])
-    #         data[locationId]['latest_time'] = int(item['updateTime'])
-
-    # for locationId, datum in data.items():
-    #     np.mean([c.a for c in c_list])
+    for locationId, locationData in locations.items():
+        locationId = int(locationId)
+        locationData['lat'] = float(locationData['lat'] )
+        locationData['lon'] = float(locationData['lon'] )
+        lDf = df[df.locationId == locationId]
+        maxTime = arrow.get(lDf.index.max())
+        ago_30_min = lDf.iloc[lDf.index.get_loc(
+            maxTime.shift(minutes=-30).timestamp, method='nearest')]
+        ago_15_min = lDf.iloc[lDf.index.get_loc(
+            maxTime.shift(minutes=-15).timestamp, method='nearest')]
+        latest = lDf[lDf.index==lDf.index.max()].iloc[0]
+        locationData.update({
+            "latest": {
+                "PM10": float(latest.PM10), 
+                "PM2_5": float(latest.PM2_5), 
+                "timestamp": arrow.get(latest.name).isoformat()
+                },
+            "30_mins_ago": {
+                "PM10": float(ago_30_min.PM10), 
+                "PM2_5": float(ago_30_min.PM2_5), 
+                "timestamp": arrow.get(ago_30_min.name).isoformat()
+                },
+            "15_mins_ago": {
+                "PM10": float(ago_15_min.PM10), 
+                "PM2_5": float(ago_15_min.PM2_5), 
+                "timestamp": arrow.get(ago_15_min.name).isoformat()
+                }
+        })
+        result[locationId] = locationData
     response = {
         "statusCode": 200,
         "body": json.dumps(result)
